bc5fbb7ca6124643ce003b43928bb4e31686f094
mendzel41 <146203020+mendzel41@users.noreply.github.com>
2023-10-18 15:49:51 +0200

    Add files via upload

---
+++ zadanie python/zadanie2.py
@@ -0,0 +1,7 @@
+print("gruszka")
+print("banan")
+print("kiwi")
+
+print("gruszka", end=" ")
+print("banan")
+print("kiwi")
---
+++ zadanie python/zadanie3.py
@@ -0,0 +1,9 @@
+# Program wyświetli w pierwszym wierszu dwa pierwsze owoce a pod spodem kiwi
+
+print("gruszka")
+print("banan")
+print("kiwi")
+
+print("gruszka", end=" ")
+print("banan")
+print("kiwi")
